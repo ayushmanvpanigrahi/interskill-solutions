beeb03fb448b86123da93f9e76b05f9173a88ef2
Ayushman Panigrahi <ayushmanpanigrahi2147@gmail.com>
2025-08-11 15:21:10 +0530

    second commit

---
+++ interskill-solutions/src/app/blog/blog_1/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog1() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Blog1 Page</h1>
+        </div>
+    )
+}
---
+++ interskill-solutions/src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Blog Page</h1>
+        </div>
+    )
+}
